00c59693954da7f2b348623b71580d80bbfa2ff7
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-12 04:58:10 -0800

    Added users/index_spec

---
+++ spec/views/users/index_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/users" do
+  fixtures :users, :events
+
+  it "should not include admin column by default" do
+    assigns[:users] = [users(:aaron), users(:quentin)]
+    render '/users/index'
+
+    response.should_not have_tag('th', 'Admin?')
+  end
+
+  it "should not include admin column by default" do
+    login_as(:aaron)
+    assigns[:users] = [users(:aaron), users(:quentin)]
+    render '/users/index'
+
+    response.should have_tag('th', 'Admin?')
+  end
+end
