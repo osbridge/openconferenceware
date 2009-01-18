718e779b96f11759eb697eb73ea68957f1ee8dfe
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-17 23:48:06 -0800

    Added FieldAnnotationHelper, extracted #required_field and #private_field into this.

---
+++ app/helpers/field_annotation_helper.rb
@@ -0,0 +1,9 @@
+module FieldAnnotationHelper
+  def required_field
+    content_tag :span, "*", :class => "required-field"
+  end
+
+  def private_field
+    content_tag :span, "%", :class => "private-field"
+  end
+end
--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -1,9 +1,2 @@
 module ProposalsHelper
-  def required
-    '<span class="required-field">*</span>'
-  end
-
-  def private_field
-    content_tag :span, "%", :class => "private-field"
-  end
 end
