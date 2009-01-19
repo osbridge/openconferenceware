548ce008fa5cf6593c40970a312ab9fd85fa6f18
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-19 00:45:33 -0800

    Rixed UsersHelper#fullname_and_affiliation to use '-' rather than HTML mdash so it can be used more freely.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def fullname_and_affiliation(user)
-    return [h(user.fullname), h(user.affiliation)].reject(&:blank?).join(' &mdash; ')
+    return [h(user.fullname), h(user.affiliation)].reject(&:blank?).join(' - ')
   end
 end
