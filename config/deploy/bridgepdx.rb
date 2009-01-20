aaa49c68237c92928d6d8fbc637f2c903e992026
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-19 22:31:02 -0800

    Added bridgepdx deploy script

---
+++ config/deploy/bridgepdx.rb
@@ -0,0 +1,14 @@
+set :theme, "bridgepdx"
+
+set :scm, "git"
+set :repository,  "git@github.com:igal/openconferenceware.git"
+set :branch, "master"
+set :deploy_to, "/var/www/bridgepdx_ocwdemo"
+set :user, "igal"
+set :host, "burnside"
+
+set :deploy_via, :remote_cache
+role :app, host
+role :web, host
+role :db,  host, :primary => true
+default_run_options[:pty] = true
