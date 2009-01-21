ca2d9c3856b512b3b43843fcbaa61e1927d622a2
Reid Beels <mail@reidbeels.com>
2009-01-21 01:54:13 -0800

    Added basic tagging to proposals.

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -34,6 +34,8 @@ class Proposal < ActiveRecord::Base
   has_many :comments
   has_and_belongs_to_many :users
 
+  acts_as_taggable_on :tags
+
   # Validations
   validates_presence_of :title, :description, :event_id
   validates_acceptance_of :agreement,                     :accept => true, :message => "must be accepted"
