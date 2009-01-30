df2cc6c1ec45e5a390a3096fc0010bb5198f3684
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-30 04:57:18 -0800

    Improved users and proposals, made the biography and description into Textile fields. Added helpers to display Textile data and link to help.

---
+++ app/helpers/display_textile_for_helper.rb
@@ -0,0 +1,9 @@
+module DisplayTextileForHelper
+  def display_textile_for(text)
+    return auto_link(textilize(h(text)), :all, :rel => 'nofollow')
+  end
+
+  def display_textile_help_link
+    return link_to 'Formatting', 'http://hobix.com/textile/quick.html', :popup => true
+  end
+end
