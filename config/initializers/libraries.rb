2ccabbffa8ae0b5180a96c1f95e5ad22b84e6cd9
Igal Koshevoy <igal@pragmaticraft.com>
2009-03-30 17:28:16 -0700

    Added active_record_quoting_fix for PostgreSQL.

--- config/initializers/libraries.rb
+++ config/initializers/libraries.rb
@@ -8,6 +8,7 @@ require 'facets/kernel/with'
 # Local libraries
 require 'lib/ext/active_support_cache_filestore'
 require 'lib/ext/object_logit'
+require 'lib/ext/active_record_quoting_fix'
 
 # Local gems
 require 'RedCloth' # Make #textilize will work w/ RedCloth 4.1.x
