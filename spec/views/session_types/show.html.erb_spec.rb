caac454e3cd1bb582dad273d8c4dba7ad3f10aba
Igal Koshevoy <igal@pragmaticraft.com>
2009-03-30 18:34:16 -0700

    Fixed session_types#show view spec.

--- spec/views/session_types/show.html.erb_spec.rb
+++ spec/views/session_types/show.html.erb_spec.rb
@@ -4,6 +4,13 @@ describe "/session_types/show.html.erb" do
   include SessionTypesHelper
   before(:each) do
     assigns[:session_type] = @session_type = stub_model(SessionType)
+
+    @event = stub_model(Event,
+      :id => 1,
+      :title => "Event 1"
+    )
+
+    @controller.stub!(:get_current_event_and_assignment_status).and_return([@event, :assigned_to_current])
   end
 
   it "should render attributes in <p>" do
