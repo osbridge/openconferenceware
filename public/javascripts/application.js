56d65c5dc4f0520d4c2aff2262feecca13f934e3
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-01 07:32:02 -0700

    Improved application.js, added `hash_keys` and and `array_to_hash`.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,6 +15,28 @@ $(document).ready(function() {
 
 /*===[ custom functions ]=============================================*/
 
+/*---[ data structure conveniences ]----------------------------------*/
+
+// Return an array of keys in +hash+. Example:
+//  hash_keys({'foo': 'bar', 'baz': 'qux'}); // ['foo', 'baz']
+function hash_keys(hash) {
+  array = new Array();
+  for (key in hash) {
+    array.push(key);
+  }
+  return array;
+}
+
+// Return hash where each key is an element of the +array+. Example:
+//    array_to_hash(['foo', 'bar']); // {'foo': true, 'bar': true}
+function array_to_hash(array) {
+  hash = new Array();
+  for (i in array) {
+    hash[array[i]] = true;
+  }
+  return hash;
+}
+
 /*---[ page_spinner ]-------------------------------------------------*/
 
 // Display a big spinner in the middle of the page.
