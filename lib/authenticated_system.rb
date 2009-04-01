f276940749c0078d01a18e449ac4761c0ff9fc86
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-01 01:56:42 -0700

    Fixed AuthenticatedSystem#redirect_back_or_default clear the session[:return_to] before doing the redirect, thus making sure it gets cleared.

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -90,8 +90,8 @@ module AuthenticatedSystem
     # Redirect to the URI stored by the most recent store_location call or
     # to the passed default.
     def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
       session[:return_to] = nil
+      redirect_to(session[:return_to] || default)
     end
 
     alias_method :redirect_back_or_to, :redirect_back_or_default
