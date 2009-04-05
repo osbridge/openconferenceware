8f36124557f727d50ca1eba9fec7bd383525df4c
Don Park <don@donpark.org>
2009-04-05 10:33:35 -0700

    Fixed dependencies, add RedCloth.

--- config/initializers/libraries.rb
+++ config/initializers/libraries.rb
@@ -9,6 +9,3 @@ require 'facets/kernel/with'
 require 'lib/ext/active_support_cache_filestore'
 require 'lib/ext/object_logit'
 
-# Local gems
-require 'RedCloth' # Make #textilize will work w/ RedCloth 4.1.x
-
