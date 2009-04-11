002ba942b97000789ca676af48c1ba5ae774e0f0
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-11 01:56:38 -0700

    Improved SettingsCheckers, added #always_accept_proposal_comments?.

--- app/mixins/settings_checkers_mixin.rb
+++ app/mixins/settings_checkers_mixin.rb
@@ -21,6 +21,7 @@ module SettingsCheckersMixin
   module Methods
     # Create methods like +#event_tracks?+ as wrappers for +SETTINGS.have_event_tracks+.
     %w[
+      always_accept_proposal_comments
       anonymous_proposals
       event_tracks
       event_session_types
