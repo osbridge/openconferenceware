320a6994f902827d48b23782d213cfeb3dec7541
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-12 05:17:43 -0700

    Improved authentication_steps, added #login_as and #logout.

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,11 +1,23 @@
-Given /^I am logged in as "([^\"]*)"$/ do |login|
-  @user = User.find(Fixtures.identify(login))
-  post browser_session_path, :login_as => @user.login
-  session[:user].should == @user.id
+def login_as(login)
+  if login.blank?
+    logout
+  else
+    @user = User.find(Fixtures.identify(login))
+    post browser_session_path, :login_as => @user.login
+    session[:user].should == @user.id
+  end
 end
 
-Given /^I am not logged in$/ do
+def logout
   @user = nil
   delete logout_path
   session[:user].should == nil
+end
+
+Given /^I am logged in as "([^\"]*)"$/ do |login|
+  login_as(login)
+end
+
+Given /^I am not logged in$/ do
+  logout
 end
