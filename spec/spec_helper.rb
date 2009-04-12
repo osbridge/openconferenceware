b3f16d33c3e164496d93c2af41994fd121e1ae80
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-12 01:01:36 -0700

    Extracted spec_helper.rb's customizations into spec_helper_customizations.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,28 +42,8 @@ Spec::Runner.configure do |config|
   # config.mock_with :rr
   #
   # == Notes
-  #
-  # For more information take a look at Spec::Example::Configuration and Spec::Runner
+  # 
+  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end
 
-#---[ Customizations ]--------------------------------------------------
-
-include AuthenticatedTestHelper
-
-# Save the response.body to "/tmp/response.html", to aid manual debugging.
-def save_body
-  filename = "/tmp/response.html"
-  bytes = File.open(filename, "w+"){|h| h.write(response.body)}
-  return [filename, bytes]
-end
-
-# Stub the +controller+ to provide the current event. If the +event+ or
-# +status+ aren't provided, reasonable defaults will be used.
-def stub_current_event!(opts={})
-  controller = opts[:controller] || @controller
-  event = opts[:event] || stub_model(Event, :id => 1, :title => "Event 1")
-  status = opts[:status] || :assigned_to_current
-  controller.stub!(:get_current_event_and_assignment_status).and_return([event, status])
-  assigns[:event] = event
-  return event
-end
+require File.dirname(__FILE__) + "/spec_helper_customizations"
