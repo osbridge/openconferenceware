30b6a48e29890f2ab06468d3570001cca2e1fb5c
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-12 01:01:13 -0700

    Updated spec_helper's dependency loaders to use new rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'spec'
+ENV["RAILS_ENV"] ||= 'test'
+require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+require 'spec/autorun'
 require 'spec/rails'
 
 Spec::Runner.configure do |config|
