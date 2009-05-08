3bc86e85fefa769832f26defad3f02d480526a1a
Igal Koshevoy <igal@pragmaticraft.com>
2009-05-08 04:24:31 -0700

    Implemented Schedule, Schedulable, ScheduleSection, ScheduleSlice, ScheduleBlock. Provided fixtures and demo script to exercise classes.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ class Schedule
   attr_accessor :items
 
   def initialize(event_or_items)
-    self.items    = []
+    self.items = []
 
     case event_or_items
     when Event
@@ -12,6 +12,7 @@ class Schedule
     else
       raise TypeError, "Unknown type: #{event_or_items.class.name}"
     end
+    self.items = self.items.sort_by{|item| [item.start_time, item.end_time]}
   end
 
   def days
@@ -49,7 +50,7 @@ module Schedulable
         raise ArgumentError, "End time cannot be set without a start time" unless @start_time
         raise ArgumentError, "Duration cannot be negative" if value < 0
         @duration = value
-        @end_time = @start_time + value
+        @end_time = @start_time + value.minutes
       end
 
       def overlaps?(object)
@@ -71,9 +72,9 @@ class ScheduleDay
   def self.new_array_from(items)
     returning([]) do |days|
       items.group_by{|item| item.start_time.to_date}.each do |date, collection|
-        days << ScheduleDay.new(collection, date)
+        days << self.new(collection, date)
       end
-    end
+    end.sort_by(&:date)
   end
 
   def sections
@@ -99,35 +100,62 @@ class ScheduleSection
           section.start_time = item.start_time if section.start_time > item.start_time
           section.end_time = item.end_time if section.end_time < item.end_time
         else
-          section = ScheduleSection.new([item], item.start_time, item.end_time)
-          sections << section
+          sections << self.new([item], item.start_time, item.end_time)
         end
       end
-    end
+    end.sort_by{|section| [section.start_time, section.end_time, section.items.first.title]}
   end
 
   def slices
-    @slices ||= SectionSlice.new_array_from(self.items)
+    @slices ||= ScheduleSlice.new_array_from(self.items)
   end
 end
 
 class ScheduleSlice
   include Schedulable
   attr_accessor :items
+  attr_accessor :blocks
 
-  def initialize(items=[], start_time=nil, end_time=nil)
+  def initialize(items=[], start_time=nil, end_time=nil, blocks=[])
     self.items = items
     self.start_time = start_time
     self.end_time = end_time
+    self.blocks = blocks.empty? ? ScheduleBlock.new_array_from(self.items) : blocks
   end
 
   def self.new_array_from(items)
-    raise NotImplementedError # FIXME
+    returning([]) do |slices|
+      for block in ScheduleBlock.new_array_from(items)
+        if slice = slices.find{|slice| ! slice.overlaps?(block)}
+          slice.blocks << block
+          slice.items += block.items
+          slice.start_time = block.start_time if slice.start_time > block.start_time
+          slice.end_time = block.end_time if slice.end_time < block.end_time
+        else
+          slices << self.new(block.items, block.start_time, block.end_time, [block])
+        end
+      end
+    end.sort_by{|slice| [slice.start_time, slice.end_time, slice.items.first.title]}
   end
 end
 
 class ScheduleBlock
   include Schedulable
+  attr_accessor :items
+
+  def initialize(items=[], start_time=nil, end_time=nil)
+    self.items = items.sort_by{|item| [item.start_time, item.end_time, item.title]}
+    self.start_time = start_time
+    self.end_time = end_time
+  end
+
+  def self.new_array_from(items)
+    returning([]) do |blocks|
+      items.group_by{|item| [item.start_time, item.end_time]}.each do |range, collection|
+        blocks << self.new(collection, range.first, range.last)
+      end
+    end.sort_by{|block| [block.start_time, block.end_time, block.items.first.title]}
+  end
 end
 
 class ScheduleItem
--- schedule_demo.rb
+++ schedule_demo.rb
@@ -1,23 +1,36 @@
+# This script exercises Schedule. Run the following commands to use it:
 =begin
 RAILS_ENV=test rake spec:db:fixtures:load
-./script/console test
-reload!; load 'schedule_demo.rb'
 RAILS_ENV=test ./script/runner schedule_demo.rb
 =end
 
 event = Event.find(2009)
-puts "* Event ##{event.id}: #{event.title}"
+puts "* Event: #{event.title}"
 $schedule = Schedule.new(event)
 for item in $schedule.items
-  puts "* Item ##{item.id}: #{item.title} -- #{item.start_time.to_s(:db)} to #{item.end_time.to_s(:db)}"
+  puts "  - EventItem: #{item.title} -- #{item.start_time.to_s(:db)} to #{item.end_time.to_s(:db)}"
 end
 for day in $schedule.days
-  puts "* Day: #{day.date.to_s}"
+  puts "  * Day: #{day.date.to_s}"
+  for item in day.items
+    puts "    - DayItem: #{item.title} -- #{item.start_time.to_s(:time)} to #{item.end_time.to_s(:time)}"
+  end
   for section in day.sections
-    puts "  * Section: #{section.start_time.to_s(:db)} to #{section.end_time.to_s(:db)}"
+    puts "    * Section: #{section.start_time.to_s(:time)} to #{section.end_time.to_s(:time)}"
     for item in section.items
-      puts "    * Item ##{item.id}: #{item.title} -- #{item.start_time.to_s(:db)} to #{item.end_time.to_s(:db)}"
+      puts "      - SectionItem: #{item.title} -- #{item.start_time.to_s(:time)} to #{item.end_time.to_s(:time)}"
+    end
+    for slice in section.slices
+      puts "      * Slice: #{section.start_time.to_s(:time)} to #{section.end_time.to_s(:time)}"
+      for item in slice.items
+        puts "        - SliceItem: #{item.title} -- #{item.start_time.to_s(:time)} to #{item.end_time.to_s(:time)}"
+      end
+      for block in slice.blocks
+        puts "        * Block: #{block.start_time.to_s(:time)} to #{block.end_time.to_s(:time)}"
+        for item in block.items
+          puts "          - BlockItem: #{item.title} -- #{item.start_time.to_s(:time)} to #{item.end_time.to_s(:time)}"
+        end
+      end
     end
   end
 end
-require 'rubygems'; require 'ruby-debug'; Debugger.start; debugger; 1 # FIXME
