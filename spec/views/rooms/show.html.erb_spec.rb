7a76c5da833a3de5ffdaf7237bb3cd60a6a8f7ac
Igal Koshevoy <igal@pragmaticraft.com>
2009-05-14 16:56:04 -0700

    Fixed rooms/show.html.erb_spec to include @event.

--- spec/views/rooms/show.html.erb_spec.rb
+++ spec/views/rooms/show.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe "/rooms/show.html.erb" do
   include RoomsHelper
   before(:each) do
     assigns[:room] = @room = stub_model(Room)
+    @event = stub_current_event!
   end
 
   it "should render attributes in <p>" do
