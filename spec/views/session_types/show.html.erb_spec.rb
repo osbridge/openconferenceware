675f46c32a790f363061dd549e48311ac72fb2bf
Reid Beels <mail@reidbeels.com>
2009-05-14 22:55:40 -0700

    Fixed failing view specs for tracks/show and session_types/show. Needed to pass template as the controller into stub_current_event!

--- spec/views/session_types/show.html.erb_spec.rb
+++ spec/views/session_types/show.html.erb_spec.rb
@@ -5,7 +5,9 @@ describe "/session_types/show.html.erb" do
   before(:each) do
     assigns[:session_type] = @session_type = stub_model(SessionType)
 
-    @event = stub_current_event!
+    @event = stub_current_event!(:controller => template)
+
+    template.stub!(:schedule_visible?).and_return(true)
   end
 
   it "should render attributes in <p>" do
--- spec/views/tracks/show.html.erb_spec.rb
+++ spec/views/tracks/show.html.erb_spec.rb
@@ -3,15 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/tracks/show.html.erb" do
   include TracksHelper
   before(:each) do
-    assigns[:event] = @event = stub_model(Event,
-      :id => 1,
-      :title => "Event 1"
-    )
+    @event = stub_current_event!(:controller => template)
     assigns[:track] = @track = stub_model(Track,
       :title => "value for title",
       :event_id => 1,
       :description => "value for description"
     )
+
+    template.stub(:schedule_visible?).and_return(true)
   end
 
   it "should render attributes in <p>" do
