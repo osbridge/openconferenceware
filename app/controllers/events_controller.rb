2892abedce47e568842df657189b791decc975b6
Igal Koshevoy <igal@pragmaticraft.com>
2009-05-21 05:57:24 -0700

    Fixed events#index and events#show, made them preserve flash across redirects.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,10 +6,12 @@ class EventsController < ApplicationController
   before_filter :normalize_event_path_or_redirect
 
   def index
+    flash.keep
     redirect_to event_proposals_path(@event) if @event
   end
 
   def show
+    flash.keep
     redirect_to event_proposals_path(@event)
   end
 end
