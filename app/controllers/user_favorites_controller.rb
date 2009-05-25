14fc92026e55b3b07f6acab7bff8c5c2fe2ae475
Reid Beels <mail@reidbeels.com>
2009-05-25 02:24:03 -0700

    Removing caching from user favorites ics export.

--- app/controllers/user_favorites_controller.rb
+++ app/controllers/user_favorites_controller.rb
@@ -19,24 +19,22 @@ class UserFavoritesController < ApplicationController
       format.xml  { render :xml => @user_favorites }
       format.json  { render :json => @user_favorites }
       format.ics {
-        view_cache_key = "favorites,user_#{@user.id}.ics"
-        data = Rails.cache.fetch_object(view_cache_key) {
-          calendar = Vpim::Icalendar.create2
-          @user_favorites.scheduled.each do |item|
-            calendar.add_event do |e|
-              e.dtstart     item.start_time
-              e.dtend       item.start_time + item.duration.minutes
-              e.summary     item.title
-              e.created     item.created_at if item.created_at
-              e.lastmod     item.updated_at if item.updated_at
-              e.description item.excerpt
-              e.url         session_url(item)
-              e.set_text    'LOCATION', item.room.name if item.room
-            end
+        calendar = Vpim::Icalendar.create2
+        @user_favorites.scheduled.each do |item|
+          calendar.add_event do |e|
+            e.dtstart     item.start_time
+            e.dtend       item.start_time + item.duration.minutes
+            e.summary     item.title
+            e.created     item.created_at if item.created_at
+            e.lastmod     item.updated_at if item.updated_at
+            e.description item.excerpt
+            e.url         session_url(item)
+            e.set_text    'LOCATION', item.room.name if item.room
           end
-          calendar.encode.sub(/CALSCALE:Gregorian/, "CALSCALE:Gregorian\nX-WR-CALNAME:#{@user.possessive_label(false)} favorites\nMETHOD:PUBLISH")
-        }
-        render :text => data
+        end
+        calendar.encode.sub(/CALSCALE:Gregorian/, "CALSCALE:Gregorian\nX-WR-CALNAME:#{@user.possessive_label(false)} favorites\nMETHOD:PUBLISH")
+
+        render :text => calendar
       }
     end
   end
