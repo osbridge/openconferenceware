42fe02a415d55fda541fb4f1c5fa8ac6e04d2bcb
Igal Koshevoy <igal@pragmaticraft.com>
2009-05-28 21:23:56 -0700

    Rewrote user_favorites/index.html.erb_spec using fixtures. Wow, it's so much clearer.

--- spec/views/user_favorites/index.html.erb_spec.rb
+++ spec/views/user_favorites/index.html.erb_spec.rb
@@ -2,28 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/user_favorites/index.html.erb" do
   include UserFavoritesHelper
+  fixtures :all
 
   before(:each) do
-    # OMFG how many objects must a man stub
-    @user = stub_model(User)
+    @proposals = proposals(:couchdb_session, :bigtable_session)
+    @user = users(:quentin)
+    @user.stub!(:favorites => @proposals)
     @event = stub_current_event!(:controller => template)
     @event.stub!(:proposal_status_published? => false)
     @event.stub!(:schedule_visible? => false)
-    @proposals = [
-      stub_model(Proposal, :id => 23, :title => "A talk", :users => [@user], :event => @event),
-      stub_model(Proposal, :id => 42, :title => "Another talk", :users => [@user], :event => @event),
-    ]
-    @container = mock(Array, :proposals => mock(Array, :all => @proposals), :id => 1)
-    @user = assigns[:user] = stub_model(User, :favorites => @container)
     template.stub!(:schedule_visible? => false)
+
+    assigns[:user] = @user
   end
 
   it "renders a list of user_favorites" do
     render "user_favorites/index.html.erb"
 
-    response.should have_tag '.proposal_row', :count => 2
-    response.should have_tag '#proposal_row_23'
-    response.should have_tag '#proposal_row_42'
+    response.should have_tag ".proposal_row", :count => 2
+    response.should have_tag "#proposal_row_#{@proposals[0].id}"
+    response.should have_tag "#proposal_row_#{@proposals[1].id}"
   end
 end
 
