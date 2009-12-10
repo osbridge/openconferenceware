8446ca1e31623d8f12db305a44927c7a1f50caec
Igal Koshevoy <igal@pragmaticraft.com>
2009-12-10 12:29:50 -0800

    Rewrote DeferProxy, simplified it to the bare essentials and added tests.

--- lib/defer_proxy.rb
+++ lib/defer_proxy.rb
@@ -56,38 +56,30 @@
 #     <%= @records.size %>
 #   <% end %>
 class DeferProxy
-  attr_accessor :__called
-  attr_accessor :__callback
-  attr_accessor :__value
+  # In order to proxy the most methods, remove this object's instance methods
+  # so that calls will be sent to #method_missing for forwarding.
+  instance_methods.each { |m| undef_method(m) unless m =~ /^__/ }
 
+  # Create proxy that, when called, will return a value by callling the block.
   def initialize(&block)
-    self.__callback = block
+    @__defer_proxy_callback = block
   end
 
-  def __materialize(method=nil, *args, &block)
-    unless self.__called
-      Rails.logger.debug("DeferProxy materialized by: #{self.__value.class.name}##{method}") if defined?(Rails)
-      self.__value = self.__callback.call
-      self.__called = true
-      return self.__value
-    end
+  # Return true if asked if this is a kind of DeferProxy else fallback to
+  # +kind_of?+ behavior of proxied object.
+  def kind_of?(klass)
+    return(klass == DeferProxy || method_missing(:kind_of?, klass))
   end
 
-  def method_missing(method, *args, &block)
-    self.__materialize(method)
-    return self.__value.send(method, *args, &block)
+  # Materialize the proxied object and call it.
+  def method_missing(name, *args, &block)
+    return(__materialize.send(name, *args, &block))
   end
 
-  alias_method :kind_of_old?, :kind_of?
-  def kind_of_with_trickery?(klass)
-    self.__materialize('kind_of?')
-    # FIXME how to handle #to_a calls?
-    # TODO Figure out why this causes a endless loop: self.kind_of_without_trickery?(DeferProxy)
-    #return self.__value.kind_of?(klass) || self.kind_of_without_trickery?(klass)
-    #return self.class == klass || self.kind_of_without_trickery?(klass) || self.__value.kind_of?(klass)
-    return self.class == klass || self.__value.kind_of?(klass)
+  # Return the proxied object, materializing it if needed.
+  def __materialize(*args, &block)
+    return(@__defer_proxy_value ||= @__defer_proxy_callback.call(*args, &block))
   end
-  alias_method_chain :kind_of?, :trickery
 end
 
 # Return a DeferProxy instance for the given +block+.
@@ -97,15 +89,39 @@ end
 
 # Return the content of a value, be it a Defer or not.
 def Undefer(value)
-  # TODO Surely there's a less hideous way!?
-  return value.respond_to?(:kind_of_with_trickery?) ? value.__materialize : value
+  return value.kind_of?(DeferProxy) ? value.__materialize : value
 end
 
-__END__
+# Self-test
+if __FILE__ == $0
+  require 'test/unit'
+  class DeferProxyTest < Test::Unit::TestCase
+    def test_should_proxy_an_integer
+      assert_equal(1, Defer{1})
+    end
 
-# Test
-load 'lib/defer_proxy.rb'
-x = Defer { [1,2,3] }
-x.each{|v| p v}
-x.kind_of? Array
-x.kind_of? DeferProxy
+    def test_should_proxy_an_array
+      assert_equal([1,2,3], Defer{[1,2,3]}.map{|t| t})
+    end
+
+    def test_should_proxy_an_array_for_use_with_map
+      assert_equal([1,2,3], Defer{[1,2,3]}.map{|t| t})
+    end
+
+    def test_should_be_able_to_use_kind_of_on_proxied_object
+      assert(Defer{[1,2,3]}.kind_of?(Array))
+    end
+
+    def test_should_be_able_to_use_kind_of_on_self
+      assert(Defer{[1,2,3]}.kind_of?(DeferProxy))
+    end
+
+    def test_should_provide_access_to_proxied_object
+      assert_equal([1,2,3], Undefer(Defer{[1,2,3]}))
+    end
+
+    def test_should_provide_access_to_proxied_object_thats_not_a_defer
+      assert(Undefer(Defer{[1,2,3]}).respond_to?(:__materialize) == false)
+    end
+  end
+end
