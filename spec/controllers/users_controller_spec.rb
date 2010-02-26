cd1009cb5c806b76e81f99887c8ab225aa893fc5
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-26 02:12:17 -0800

    Improved users_controller_spec: described behaviors for "complete_profile" and "proposals".

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -28,7 +28,6 @@ describe UsersController do
   end
 
   describe "create" do
-    
     def post_to_create(params = {})
       params.merge!({
         :user => {
@@ -37,14 +36,13 @@ describe UsersController do
       })
       post :create, params
     end
-    
+
     it "should delete the auth token cookie" do
       cookie_hash = {}
       cookie_hash.should_receive(:delete).with(:auth_token)
       @controller.should_receive(:cookies).any_number_of_times.and_return(cookie_hash)
       post_to_create
     end
-      
   end
 
   describe "update" do
@@ -126,6 +124,41 @@ describe UsersController do
 
       User.exists?(user.id).should be_false
       flash[:success].should_not be_blank
+    end
+  end
+
+  describe "complete_profile" do
+    it "should redirect a completed profile to the user's profile" do
+      user = users(:clio)
+      login_as(user)
+      user.stub!(:complete_profile => true)
+      controller.stub!(:current_user => user)
+
+      get :complete_profile, :id => user.id
+
+      response.should redirect_to(user_path(user))
+    end
+
+    it "should redirect an incomplete profile to the edit form" do
+      user = users(:clio)
+      login_as(user)
+      user.stub!(:complete_profile => false)
+      controller.stub!(:current_user => user)
+
+      get :complete_profile, :id => user.id
+
+      response.should redirect_to(edit_user_path(user, :require_complete_profile => true))
+    end
+  end
+
+  describe "proposals" do
+    it "should render proposals for a user" do
+      user = users(:clio)
+      login_as(user)
+
+      get :proposals, :id => user.id
+
+      response.should be_success
     end
   end
 
