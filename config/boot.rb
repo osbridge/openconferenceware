639bf69abc9f931b407f4a921eb3379f1ae22928
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-27 14:44:48 -0800

    Reworked boot with module/class hack for use with Passenger.

--- config/boot.rb
+++ config/boot.rb
@@ -105,20 +105,22 @@ module Rails
   end
 end
 
-# Bundler integration from http://gist.github.com/302406
-class Rails::Boot
-  def run
-    load_initializer
-    extend_environment
-    Rails::Initializer.run(:set_load_path)
-  end
+# Bundler integration from http://gist.github.com/302406 with module/class hack for use within Passenger
+module Rails
+  class Boot
+    def run
+      load_initializer
+      extend_environment
+      Rails::Initializer.run(:set_load_path)
+    end
 
-  def extend_environment
-    Rails::Initializer.class_eval do
-      old_load = instance_method(:load_environment)
-      define_method(:load_environment) do
-        Bundler.require :default, Rails.env
-        old_load.bind(self).call
+    def extend_environment
+      Rails::Initializer.class_eval do
+        old_load = instance_method(:load_environment)
+        define_method(:load_environment) do
+          Bundler.require :default, Rails.env
+          old_load.bind(self).call
+        end
       end
     end
   end
