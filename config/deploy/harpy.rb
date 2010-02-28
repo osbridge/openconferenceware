4453cf069f657e1063abcae14d0c1f12e1125164
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-27 22:40:45 -0800

    Added Capistrano stage for harpy.

---
+++ config/deploy/harpy.rb
@@ -0,0 +1,16 @@
+set :theme, "bridgepdx"
+
+set :scm, "git"
+set :repository,  "#{File.dirname(File.dirname(File.dirname(__FILE__)))}"
+set :deploy_to, "/var/www/bridgepdx_ocw"
+set :host, "harpy"
+set :user, "bridgepdx"
+
+set :deploy_via, :copy
+set :copy_cache, true
+set :copy_compression, :gzip
+set :copy_exclude, ['.git', 'log', 'tmp', '*.sql', '*.diff', 'coverage.info', 'coverage', 'public/system', 'tags', 'db/*.yml', 'db/*.sql', 'db/*.sqlite3', '.*.swp']
+
+role :app, host
+role :web, host
+role :db,  host, :primary => true
