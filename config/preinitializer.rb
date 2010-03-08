fe0792be4fdb1991b5d0d75b2075726de76a29bc
Igal Koshevoy <igal@pragmaticraft.com>
2010-03-08 11:53:13 -0800

    Fixed preinitializer to work with Bundler 0.9.10.

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -6,7 +6,7 @@ rescue LoadError
   # Fallback on doing the resolve at runtime.
   require "rubygems"
   require "bundler"
-  if Bundler::VERSION <= "0.9.5"
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
     raise RuntimeError, "Bundler incompatible.\n" +
       "Your bundler version is incompatible with Rails 2.3 and an unlocked bundle.\n" +
       "Run `gem install bundler` to upgrade or `bundle lock` to lock."
