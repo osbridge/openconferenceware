b66e11a729d2f2ef09d894a142fcb2fe1dc77cbc
Igal Koshevoy <igal@pragmaticraft.com>
2010-03-14 15:07:05 -0700

    Fixed #my_truncate: added logic for coping with blank strings, wrote spec.

--- app/helpers/my_truncate_helper.rb
+++ app/helpers/my_truncate_helper.rb
@@ -1,6 +1,7 @@
 module MyTruncateHelper
   # Just like #truncate, but works.
   def my_truncate(string, length=30, omission='...')
+    return nil if string.blank?
     chars = string.chars
     if chars.length > length
       return (string.chars[0...(length - omission.chars.length)].to_s + '...')
