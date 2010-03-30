faceed09247d2520d83fed12260efa4791ea890e
Igal Koshevoy <igal@pragmaticraft.com>
2010-03-30 00:35:02 -0700

    Added :date_time_long formatting string.

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -2,6 +2,7 @@ date_time_formats = {
   :default => '%m/%d/%Y',
   :date_time12  => "%m/%d/%Y %I:%M%p",
   :date_time24  => "%m/%d/%Y %H:%M",
+  :date_time_long => "%A, %B %d, %Y %I:%M%p",
 }
 
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(date_time_formats)
