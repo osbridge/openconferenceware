5c7e90515ed254245995a2b84fa219e0e5f0b96f
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-26 06:48:19 -0700

    Fixed date_time_formats initializer to defautl to providing a time in a date-time. Unsure why this was ever different.

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,5 +1,5 @@
 date_time_formats = {
-  :default => '%m/%d/%Y',
+  :default => '%m/%d/%Y %I:%M%p',
   :date_time12  => "%m/%d/%Y %I:%M%p",
   :date_time24  => "%m/%d/%Y %H:%M",
   :date_time_long => "%A, %B %d, %Y %I:%M%p",
