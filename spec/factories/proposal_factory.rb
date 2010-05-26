45de04846f5a678f45fbc6e41ebcda7109e302fb
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-26 02:06:57 -0700

    Added #proposal_for_event and #session_for_event test helpers.

--- spec/factories/proposal_factory.rb
+++ spec/factories/proposal_factory.rb
@@ -1,3 +1,29 @@
+# Return a proposal for the +event+, using it's session_types, tracks and
+# rooms. Accepts an optional +opts+ which is passed to the :proposal factory.
+#
+# Example:
+#   event = Factory :populated_event
+#   proposal = proposal_for_event(event)
+#   proposal2 = proposal_for_event(event, :user => (Factory :user))
+def proposal_for_event(event, opts={})
+  options = {
+    :event => event,
+    :session_type => event.session_types.first,
+    :track => event.tracks.first,
+    :room => event.rooms.first
+  }.merge(opts)
+
+  return Factory :proposal, options
+end
+
+def session_for_event(event, opts={})
+  proposal = proposal_for_event(event, opts)
+  proposal.status = "confirmed"
+  proposal.users << Factory(:user) if opts[:users].nil?
+  proposal.save!
+  return proposal
+end
+
 # NOTE: A proposal's relationship to it's user profile information is
 # configurable. If `SETTINGS.have_user_profiles` is false, then the proposal
 # contains the user profile information (e.g., presenter's name). However,
