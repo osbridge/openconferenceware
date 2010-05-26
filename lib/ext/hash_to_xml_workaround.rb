b3f54497584d3f0105612d303f0ef537a4824ca8
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-26 09:11:07 -0700

    Added Hash#to_xml_workaround library to allow hashes to emit structures that ActiveSupport#to_xml can serialize.

---
+++ lib/ext/hash_to_xml_workaround.rb
@@ -0,0 +1,12 @@
+class Hash
+  def to_xml_workaround(*args)
+    returning(self.clone) do |result|
+      result.each_pair do |key, value|
+        case result[key]
+        when Array
+          result[key] = value.each_with_index.to_a.inject({}){|s,v| s["index_#{v.last}"] = v.first; s}
+        end
+      end
+    end
+  end
+end
