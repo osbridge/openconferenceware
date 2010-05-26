644e1e56edcff42d7aa9f9f11d760f5667b4c336
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-26 03:47:35 -0700

    Fixed proposals#show to display the scheduled time of a confirmed and published session even if it has no location.

--- spec/views/proposals/show.html.erb_spec.rb
+++ spec/views/proposals/show.html.erb_spec.rb
@@ -23,20 +23,8 @@ describe "/proposals/show.html.erb" do
       assigns[:kind] = :proposal
     
       render "/proposals/show.html.erb"
-      response.should_not have_tag("div.proposal-status #{status}")
+      response.should_not have_tag(".proposal-status #{status}")
     end
-  end
-  
-  it "should show the proposal status for a confirmed proposal if statuses are published" do
-    @event.proposal_status_published = true
-    @proposal.status = 'confirmed'
-    
-    assigns[:event]  = @event
-    assigns[:proposal] = @proposal
-    assigns[:kind] = :proposal
-    
-    render "/proposals/show.html.erb"
-    response.should have_tag("div.proposal-status")
   end
   
   %w[accepted rejected junk].each do |status|
@@ -49,7 +37,44 @@ describe "/proposals/show.html.erb" do
       assigns[:kind] = :proposal
     
       render "/proposals/show.html.erb"
-      response.should_not have_tag("div.proposal-status #{status}")
+      response.should_not have_tag(".proposal-status #{status}")
+    end
+  end
+
+  describe "with confirmed proposal for event that publishes status" do
+    before :each do
+      @event.proposal_status_published = true
+      @proposal.status = 'confirmed'
+      @proposal.start_time = nil
+      @proposal.room = nil
+
+      assigns[:event]  = @event
+      assigns[:proposal] = @proposal
+      assigns[:kind] = :proposal
+    end
+
+    it "should show the proposal status for a confirmed proposal" do
+      render "/proposals/show.html.erb"
+      response.should have_tag(".proposal-status")
+      response.should_not have_tag(".proposal-scheduling")
+      response.should_not have_tag(".proposal-room")
+    end
+
+    it "should show session time if set" do
+      @proposal.start_time = Time.now
+
+      render "/proposals/show.html.erb"
+      response.should have_tag(".proposal-scheduling")
+      response.should_not have_tag(".proposal-room")
+    end
+
+    it "should show session time and location if both set" do
+      @proposal.start_time = Time.now
+      @proposal.room = @event.rooms.first
+
+      render "/proposals/show.html.erb"
+      response.should have_tag(".proposal-scheduling")
+      response.should have_tag(".proposal-room")
     end
   end
 end
