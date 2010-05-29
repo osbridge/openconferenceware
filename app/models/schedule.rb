5f69b834ea0736775666c525b943514967ac1d05
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-28 20:25:29 -0700

    Fixed Schedule#initialize to use actual times for sorting, not strings.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -14,7 +14,10 @@ class Schedule
     else
       raise TypeError, "Unknown type: #{event_or_items.class.name}"
     end
-    self.items = self.items.reject{|item| item.end_time.nil?}.sort_by{|item| [item.start_time, item.end_time].join('.')}
+    self.items = self.items.reject{|item| item.end_time.nil?}.sort_by do |item|
+      sorter = [item.start_time]
+      sorter << item.end_time if item.end_time
+    end
   end
 
   def days
