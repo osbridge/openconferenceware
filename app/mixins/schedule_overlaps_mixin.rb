e7b743aa8fff14f0518f9139d0a76c6be05771b0
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-28 18:44:50 -0700

    Improved ScheduleOverlapsMixin to cope with items that have no duration or the same start and end time.

--- app/mixins/schedule_overlaps_mixin.rb
+++ app/mixins/schedule_overlaps_mixin.rb
@@ -1,6 +1,10 @@
 module ScheduleOverlapsMixin
   def overlaps?(object)
     raise ArgumentError unless object.respond_to?(:start_time) && object.respond_to?(:end_time)
-    (self.start_time..self.end_time).overlaps?(object.start_time..object.end_time) && (self.end_time != object.start_time && self.start_time != object.end_time)
+    if self.start_time != self.end_time && object.start_time != object.end_time
+      (self.start_time..self.end_time).overlaps?(object.start_time..object.end_time) && (self.end_time != object.start_time && self.start_time != object.end_time)
+    else
+      self.start_time == object.start_time
+    end
   end
 end
