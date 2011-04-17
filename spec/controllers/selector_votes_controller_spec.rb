62e1b22696baa02ba258ca4513a15c6945fa206e
Igal Koshevoy <igal@pragmaticraft.com>
2011-04-17 10:29:39 -0700

    Add SelectorVotesController specs.

--- spec/controllers/selector_votes_controller_spec.rb
+++ spec/controllers/selector_votes_controller_spec.rb
@@ -6,6 +6,125 @@ describe SelectorVotesController do
     @mock_selector_vote ||= mock_model(SelectorVote, stubs)
   end
 
+  describe "GET index" do
+    describe "when logged in as admin" do
+      before do
+        Event.destroy_all # Previous tests may have left an event behind
+
+        @user = Factory :admin
+        login_as @user
+      end
+
+      describe "without an event" do
+        before do
+          get :index
+        end
+
+        it "should redirect to root path" do
+          response.should redirect_to(root_path)
+        end
+
+        it "should notify user of error" do
+          flash[:failure].should =~ /Can't display selector votes/
+        end
+      end
+
+      describe "with an event" do
+        before do
+          @user1 = Factory :selector
+          @user2 = Factory :selector
+
+          @event = Factory :populated_event
+
+          @proposal1 = proposal_for_event(@event)
+          @proposal2 = proposal_for_event(@event)
+
+          @selector_vote1 = @proposal1.selector_votes.create :user => @user1, :rating => 1, :comment => "Meh."
+          @selector_vote2 = @proposal1.selector_votes.create :user => @user2, :rating => 5, :comment => "Yay!"
+        end
+
+        shared_examples_for "HTML and CSV" do
+          it "should have event" do
+            assigns[:event].should == @event
+          end
+
+          it "should have proposals" do
+            assigns[:proposals].size.should == 2
+          end
+
+          it "should include proposals without selector votes" do
+            assigns[:proposals].should include(@proposal2)
+          end
+
+          it "should include proposals with selector votes" do
+            assigns[:proposals].should include(@proposal1)
+          end
+
+          it "should include selector votes for a proposal" do
+            proposal = assigns[:proposals].find { |o| o == @proposal1 }
+            selector_vote1 = proposal.selector_votes.should include(@selector_vote1)
+            selector_vote2 = proposal.selector_votes.should include(@selector_vote2)
+          end
+        end
+
+        describe "requesting HTML" do
+          before do
+            get :index, :event_id => @event.slug
+          end
+
+          it_should_behave_like "HTML and CSV"
+
+          it "should render successfully" do
+            response.should be_success
+          end
+
+          # NOTE: The assiciated view spec is very complete: spec/views/selector_votes/index.html.erb_spec.rb
+        end
+
+        describe "requesting CSV" do
+          before do
+            get :index, :event_id => @event.slug, :format => 'csv'
+            @struct = FasterCSV.parse(response.body)
+            @row_for_proposal1 = @struct.find{|row| row.first == @proposal1.id.to_s}
+            @row_for_proposal2 = @struct.find{|row| row.first == @proposal2.id.to_s}
+            @column_with_selector_points = @struct.first.rindex{|column| column == "Selector points"}
+          end
+
+          it_should_behave_like "HTML and CSV"
+
+          it "should render successfully" do
+            response.should be_success
+          end
+
+          it "should contain a header" do
+            @struct.first.should include("Id")
+          end
+
+          it "should contain a column with selector points" do
+            @column_with_selector_points.should > 0
+          end
+
+          it "should contain proposal with selector votes" do
+            @row_for_proposal1.should_not be_blank
+          end
+
+          it "should have correct selector points for proposal with selector votes" do
+            @row_for_proposal1[@column_with_selector_points].to_i.should == @proposal1.selector_vote_points
+          end
+
+          it "should contain proposal without selector votes" do
+            @row_for_proposal2.should_not be_blank
+          end
+
+          it "should have zero selector points for proposal without selector votes" do
+            @row_for_proposal2[@column_with_selector_points].to_i.should == 0
+          end
+        end
+      end
+    end
+
+  end
+
   describe "POST create" do
 
     describe "when not logged in" do
--- spec/views/selector_votes/index.html.erb_spec.rb
+++ spec/views/selector_votes/index.html.erb_spec.rb
@@ -4,13 +4,40 @@ describe "/selector_votes/index.html.erb" do
   include SelectorVotesHelper
 
   before(:each) do
-    assigns[:selector_votes] = [
-      stub_model(SelectorVote),
-      stub_model(SelectorVote)
-    ]
+    @user1 = Factory :selector
+    @user2 = Factory :selector
+
+    @event = Factory :populated_event
+
+    @proposal1 = proposal_for_event(@event)
+    @proposal2 = proposal_for_event(@event)
+
+    @selector_vote1 = @proposal1.selector_votes.create :user => @user1, :rating => 1, :comment => "Meh."
+    @selector_vote2 = @proposal1.selector_votes.create :user => @user2, :rating => 5, :comment => "Yay!"
+
+    @comment1 = @proposal1.comments.create :email => "foo@.bar.com", :message => "Hi!"
+
+    assigns[:event] = @event
+    assigns[:proposals] = [@proposal1, @proposal2]
+
+    render
   end
 
-  it "renders a list of selector_votes" do
-    render
+  describe "result" do
+    it "should include proposal with selector votes" do
+      have_tag ".proposal_#{@proposal1.id}"
+    end
+
+    it "should include proposal without selector votes" do
+      have_tag ".proposal_#{@proposal2.id}"
+    end
+
+    it "should include selector vote for a proposal" do
+      have_tag ".proposal_#{@proposal1.id} .selector_vote_#{@selector_vote1}"
+    end
+
+    it "should include comment for a proposal" do
+      have_tag ".proposal_#{@proposal1.id} .comment_#{@comment1}"
+    end
   end
 end
