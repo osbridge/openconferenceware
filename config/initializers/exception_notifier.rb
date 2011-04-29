cedf17f734b35df6b36ead0ee50f7c6775a32458
Igal Koshevoy <igal@pragmaticraft.com>
2011-04-29 13:44:54 -0700

    Improve exception notifier to send email from address it sends to, making it easy to reply to alerts.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,2 +1,2 @@
 ExceptionNotifier.exception_recipients = [SECRETS.administrator_email]
-ExceptionNotifier.sender_address = "error@#{Socket.gethostname}"
+ExceptionNotifier.sender_address = "#{SECRETS.administrator_email}"
