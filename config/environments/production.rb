039852f00aba07ac29ee29f077bf2a9faf7ce174
Reid Beels <mail@reidbeels.com>
2012-04-27 13:45:57 -0700

    Don't set the delivery_method in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,5 +20,3 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-
-config.action_mailer.delivery_method = :emailthing
