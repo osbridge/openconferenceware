7262a9b9ec1fc9b9aefe29fca10c621dbdc2446b
Igal Koshevoy <igal@pragmaticraft.com>
2012-06-26 03:44:22 -0700

    Improve SerializersMixin to support objects without public_attributes.

--- app/mixins/serializers_mixin.rb
+++ app/mixins/serializers_mixin.rb
@@ -1,9 +1,16 @@
+# = SerializersMixin
+#
+# Mixin to ActiveRecord model classes to provide working and sanitized #to_json and #to_xml methods.
 module SerializersMixin
+  def to_public_attributes
+    self.respond_to?(:public_attributes) ? self.public_attributes : self.attributes
+  end
+
   def to_xml(*args)
-    self.public_attributes.to_xml_workaround.to_xml(*args)
+    self.to_public_attributes.to_xml_workaround.to_xml(*args)
   end
 
   def to_json(*args)
-    self.public_attributes.to_json(*args)
+    self.to_public_attributes.to_json(*args)
   end
 end
