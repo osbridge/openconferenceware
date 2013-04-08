a311b24bbbcf9ca5f61ea40c9e2afdc23ca132b2
Reid Beels <mail@reidbeels.com>
2013-04-07 23:58:53 -0700

    Rails 2.2: String#chars => String#mb_chars

--- app/helpers/my_truncate_helper.rb
+++ app/helpers/my_truncate_helper.rb
@@ -2,9 +2,9 @@ module MyTruncateHelper
   # Just like #truncate, but works.
   def my_truncate(string, length=30, omission='...')
     return nil if string.blank?
-    chars = string.chars
+    chars = string.mb_chars
     if chars.length > length
-      return (string.chars[0...(length - omission.chars.length)].to_s + '...')
+      return (string.mb_chars[0...(length - omission.mb_chars.length)].to_s + '...')
     else
       return string
     end
