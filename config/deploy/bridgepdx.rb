eacbdc4cf5c7cebdbfc43a63c6325c5ce5b2f063
Kirsten Comandich <kcomandich@gmail.com>
2013-04-12 19:51:41 -0700

    Change repository to osbridge.

--- config/deploy/bridgepdx.rb
+++ config/deploy/bridgepdx.rb
@@ -2,7 +2,7 @@ set :theme, "bridgepdx"
 
 set :scm, "git"
 set :branch, "master" unless variables[:branch]
-set :repository,  "git://github.com/igal/openconferenceware.git"
+set :repository,  "git://github.com/osbridge/openconferenceware.git"
 set :deploy_to, "/var/www/bridgepdx_ocw"
 set :host, "opensourcebridge.org"
 set :user, "bridgepdx"
