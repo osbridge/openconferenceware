b1a4bd15a8a857c0de78d2456dd35df29d9dce96
Reid Beels <mail@reidbeels.com>
2013-04-23 14:52:27 -0700

    Include both accepted and confirmed proposals in schedule for admins

--- app/models/event.rb
+++ app/models/event.rb
@@ -127,10 +127,9 @@ class Event < ActiveRecord::Base
   def calendar_items(is_admin=false)
     results = []
     if self.schedule_published? || is_admin
+      results += self.proposals.confirmed.scheduled.find(:all, :include => [:users, :room, :session_type, {:track => :event}])
       if is_admin
         results += self.proposals.accepted.scheduled.find(:all, :include => [:users, :room, :session_type, {:track => :event}])
-      else
-        results += self.proposals.confirmed.scheduled.find(:all, :include => [:users, :room, :session_type, {:track => :event}])
       end
     elsif is_admin
     end
