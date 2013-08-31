6aeed39fc6db9674602dd2934b425a68899b6935
Reid Beels <mail@reidbeels.com>
2013-08-30 18:24:11 -0700

    Translate rails flash keys to bootstrap classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  FLASH_KEY_TRANSLATION = {
+    "notice" => "info",
+    "failure" => "danger",
+    "success" => "success"
+  }
   def notice
-    # XXX Replace with flash conductor plugin?
     unless flash.blank?
-      content_tag("div", :id => "flash", :class => "flash") do
-        flash.keys.map do |key|
-          content_tag("p", flash[key], :class => key)
-        end.join("\n")
-      end
+      flash.keys.map do |key|
+        content_tag("div", flash[key], :class => "alert alert-#{FLASH_KEY_TRANSLATION[key.to_s]}")
+      end.join("\n")
     end
   end
 
