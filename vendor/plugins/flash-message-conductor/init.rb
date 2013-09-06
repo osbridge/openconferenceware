cda99c5e317a6b6e10a55441d960812e18035397
Reid Beels <mail@reidbeels.com>
2013-09-06 16:52:12 -0700

    Remove flash-message-conductor and yaml_db plugins
    We weren't actually using these

--- vendor/plugins/flash-message-conductor/init.rb
+++
@@ -1,7 +0,0 @@
-# Include hook code here
-require File.dirname(__FILE__) + '/lib/flash_message_conductor'
-
-ActionController::Base.send( :include, PlanetArgon::FlashMessageConductor::ControllerHelpers )
-ActionView::Base.send( :include, PlanetArgon::FlashMessageConductor::ViewHelpers )
-
-
