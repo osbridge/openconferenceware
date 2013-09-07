dd35755a98eeaae839e32a2b41c805acbcc9d9fe
Reid Beels <mail@reidbeels.com>
2013-09-07 01:11:19 -0700

    Rails 3: Move parameter filtering into application.rb

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module OpenConferenceWare
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     config.before_initialize do
       # Read secrets
