b89da7923241c81017d6e9116d78dcd4bf4bb3b9
Reid Beels <mail@reidbeels.com>
2013-09-09 10:42:21 -0700

    Rspec 2: Upgrade routing specs

--- spec/controllers/sessions_routing_spec.rb
+++ spec/controllers/sessions_routing_spec.rb
@@ -1,18 +1,10 @@
 require 'spec_helper'
 
 describe ProposalsController do
-  describe "route generation" do
-
-    it "should map { :controller => 'proposals', :action => 'sessions_index' } to /sessions" do
-      route_for(:controller => "proposals", :action => "sessions_index").should == "/sessions"
-    end
-  
-  end
-
   describe "route recognition" do
 
     it "should generate params { :controller => 'proposals', action => 'sessions_index' } from GET /sessions" do
-      params_from(:get, "/sessions").should == {:controller => "proposals", :action => "sessions_index"}
+      {:get => "/sessions"}.should route_to(:controller => "proposals", :action => "sessions_index")
     end
       
   end
