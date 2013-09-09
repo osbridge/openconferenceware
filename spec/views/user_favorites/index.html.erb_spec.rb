8716dd8ea60312d86ddd15a512239d5e8d7d2cc0
Reid Beels <mail@reidbeels.com>
2013-09-09 13:02:03 -0700

    Mark user-related view specs that hang the test run as pending

--- spec/views/user_favorites/index.html.erb_spec.rb
+++ spec/views/user_favorites/index.html.erb_spec.rb
@@ -18,6 +18,8 @@ describe "user_favorites/index.html.erb" do
   end
 
   it "renders a list of user_favorites" do
+    pending "FIXME: Hangs the test suite when run as part of a batch"
+
     render
 
     rendered.should have_selector(".proposal_row", :count => 2)
--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -4,6 +4,8 @@ describe "users/index.html.erb" do
   fixtures :users, :events
 
   it "should not include admin column by default" do
+    pending "FIXME: Hangs the test suite when run as a whole"
+
     assign(:users, [users(:aaron), users(:quentin)])
     render
 
@@ -11,6 +13,8 @@ describe "users/index.html.erb" do
   end
 
   it "should include admin column when admin is logged in" do
+    pending "FIXME: Hangs the test suite when run as a whole"
+
     login_as(:aaron)
     assign(:users, [users(:aaron), users(:quentin)])
     render
