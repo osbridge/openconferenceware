2b26fb679210b7d2cb414cf2493197fa73cb9ca7
Reid Beels <mail@reidbeels.com>
2013-09-09 11:53:38 -0700

    Rspec 2: Wrap spec helper methods in a module

--- spec/spec_helper_customizations.rb
+++ spec/spec_helper_customizations.rb
@@ -16,40 +16,46 @@ Dir["#{Rails.root}/spec/factories/*.rb"].each{|filename| require filename}
 
 #---[ Functions ]-------------------------------------------------------
 
-# Save the response.body to "/tmp/response.html", to aid manual debugging.
-def save_body
-  filename = "/tmp/response.html"
-  bytes = File.open(filename, "w+"){|h| h.write(response.body)}
-  return [filename, bytes]
+module OCWHelpers
+  # Save the response.body to "/tmp/response.html", to aid manual debugging.
+  def save_body
+    filename = "/tmp/response.html"
+    bytes = File.open(filename, "w+"){|h| h.write(response.body)}
+    return [filename, bytes]
+  end
+
+  # Stub the +controller+ to provide the current event. If the +event+ or
+  # +status+ aren't provided, reasonable defaults will be used.
+  #
+  # Options:
+  # * :event => Event object to use, else a mock will be generated.
+  # * :events => Array of events to use, else the :event or its mock will be used.
+  # * :status => Assignment status to use, else :assigned_to_current will be used.
+  def stub_current_event!(opts={})
+    controller = opts[:controller] || @controller
+    event = opts[:event] || stub_model(Event, :id => 1, :title => "Current Event", :slug => 'current')
+    events = opts[:events] || [event]
+    status = opts[:status] || :assigned_to_current
+    controller.stub!(
+      :get_current_event_and_assignment_status => [event, status],
+      :assigned_event => event,
+      :assigned_events => events)
+
+    if self.respond_to?(:assign)
+      assign(:event, event)
+      assign(:events, events)
+    end
+
+    Event.stub!(:lookup).and_return do |*args|
+      key = args.pop
+      key ? event : events
+    end
+    return event
+  end
 end
 
-# Stub the +controller+ to provide the current event. If the +event+ or
-# +status+ aren't provided, reasonable defaults will be used.
-#
-# Options:
-# * :event => Event object to use, else a mock will be generated.
-# * :events => Array of events to use, else the :event or its mock will be used.
-# * :status => Assignment status to use, else :assigned_to_current will be used.
-def stub_current_event!(opts={})
-  controller = opts[:controller] || @controller
-  event = opts[:event] || stub_model(Event, :id => 1, :title => "Current Event", :slug => 'current')
-  events = opts[:events] || [event]
-  status = opts[:status] || :assigned_to_current
-  controller.stub!(
-    :get_current_event_and_assignment_status => [event, status],
-    :assigned_event => event,
-    :assigned_events => events)
-
-  if self.respond_to?(:assign)
-    assign(:event, event)
-    assign(:events, events)
-  end
-
-  Event.stub!(:lookup).and_return do |*args|
-    key = args.pop
-    key ? event : events
-  end
-  return event
+RSpec.configure do |c|
+  c.include OCWHelpers
 end
 
 #---[ fin ]-------------------------------------------------------------
