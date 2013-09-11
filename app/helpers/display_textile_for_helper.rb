38c09c1e6f15946ce80e78b8ab667dc1a0408638
Reid Beels <mail@reidbeels.com>
2013-09-11 16:32:44 -0700

    Oops, can't call html_safe on nil

--- app/helpers/display_textile_for_helper.rb
+++ app/helpers/display_textile_for_helper.rb
@@ -1,6 +1,6 @@
 module DisplayTextileForHelper
   def display_textile_for(text)
-    return auto_link(textilize(sanitize(text)).html_safe, :all, :rel => 'nofollow').html_safe
+    return auto_link(textilize(sanitize(text || "")).html_safe, :all, :rel => 'nofollow').html_safe
   end
 
   def display_textile_help_link
