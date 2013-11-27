af7682dccdca6d49d50619778d93f1d64728b136
Reid Beels <mail@reidbeels.com>
2013-11-26 23:59:18 -0800

    Fix HTML escaping in javascript helpers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  require 'openid/store/filesystem'
-  provider :developer unless Rails.env.production?
+  require 'openid/store/filesystem' 
+  if %w[development preview].include?(Rails.env)
+    provider :developer, :fields => [:name, :email, :admin], :uid_field => :name
+  end
   provider :openid, :store => OpenID::Store::Filesystem.new(Rails.root.join('tmp'))
 end
