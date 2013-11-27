6c01d97c94b673e0401c152a900669990d14749b
Reid Beels <mail@reidbeels.com>
2013-11-26 16:00:38 -0800

    Stub user_profiles? in view specs

--- spec/spec_helper_customizations.rb
+++ spec/spec_helper_customizations.rb
@@ -62,6 +62,7 @@ module OCWHelpers
     view.stub(:current_user_is_proposal_speaker?).and_return(false)
     view.stub(:proposal_statuses?).and_return(true)
     view.stub(:multiple_presenters?).and_return(true)
+    view.stub(:user_profiles?).and_return(true)
     view.stub(:event_tracks?).and_return(true)
     view.stub(:event_session_types?).and_return(true)
     view.stub(:proposal_excerpts?).and_return(true)
