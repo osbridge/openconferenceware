da21f0540c3b58a0764f9098ec4dabed910774a8
Reid Beels <mail@reidbeels.com>
2013-12-02 14:47:35 -0800

    Precompile IE-support javascript

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module OpenConferenceWare
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    # Precompile IE-only assets
+    config.assets.precompile += ['ie.js']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
