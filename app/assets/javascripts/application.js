f7c85036ae15534d89e9fa0537b3c29390fa9875
Reid Beels <mail@reidbeels.com>
2013-12-02 14:42:47 -0800

    Include vendored JS in the asset manifest
    Previously, these scripts had been included by individual views
    and loaded on-demand.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,18 @@
 //= require jquery
 //= require jquery_ujs
-//= jquery-migrate-1.2.1
-//= bootstrap
+//= require jquery-migrate-1.2.1
+//= require jquery.scrollTo
+//= require jquery.localScroll
+//= require farbtastic
+//= require audiojs/audio.min
+//= require bootstrap
 //= require base
-//= require_tree .
+//= require util
+//= require favorites
+//= require menu
+//= require persona
+//= require proposals
+//= require schedule
+//= require spinner
 
 /*===[ fin ]==========================================================*/
