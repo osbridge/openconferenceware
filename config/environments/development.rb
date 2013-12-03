0dabcea3fee50605be15f7c4e3376b9df2c7be6a
Reid Beels <mail@reidbeels.com>
2013-12-02 20:48:23 -0800

    Add config.eager_load settings to environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ OpenConferenceWare::Application.configure do
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
 
+  config.eager_load = false
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
