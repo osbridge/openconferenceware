d3fd0db525a6d137dba360713b8ca2f82415a79a
Reid Beels <mail@reidbeels.com>
2013-12-10 17:24:33 -0800

    Refer to controllers with namespace in routing specs

--- spec/controllers/open_conference_ware/sessions_routing_spec.rb
+++ spec/controllers/open_conference_ware/sessions_routing_spec.rb
@@ -6,7 +6,7 @@ describe OpenConferenceWare::ProposalsController do
   describe "route recognition" do
 
     it "should generate params { controller: 'proposals', action => 'sessions_index' } from GET /sessions" do
-      {get: "/sessions"}.should route_to(controller: "proposals", action: "sessions_index")
+      {get: "/sessions"}.should route_to(controller: "open_conference_ware/proposals", action: "sessions_index")
     end
 
   end
