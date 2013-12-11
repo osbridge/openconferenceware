ab4b0c5341ca34c17302c61e7e3b1ea0b4e2f45a
Reid Beels <mail@reidbeels.com>
2013-12-10 16:52:07 -0800

    Expose the engine's migrations to the host app

--- lib/open_conference_ware/engine.rb
+++ lib/open_conference_ware/engine.rb
@@ -24,5 +24,14 @@ module OpenConferenceWare
       g.assets false
       g.helper false
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
   end
 end
