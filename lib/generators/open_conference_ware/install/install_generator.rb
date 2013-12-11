5c5e7da806afd4e0979bd36e057dc8c0cfab2507
Reid Beels <mail@reidbeels.com>
2013-12-11 14:34:04 -0800

    Move OmniAuth initializer into install generator

--- lib/generators/open_conference_ware/install/install_generator.rb
+++ lib/generators/open_conference_ware/install/install_generator.rb
@@ -2,7 +2,11 @@ class OpenConferenceWare::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_config_initializer
-    copy_file "config_initializer.rb", "config/initializers/open_conference_ware.rb"
+    copy_file "config_initializer.rb", "config/initializers/01_open_conference_ware.rb"
+  end
+
+  def copy_omniauth_initializer
+    copy_file "omniauth_initializer.rb", "config/initializers/02_omniauth.rb"
   end
 
   def include_engine_seeds
