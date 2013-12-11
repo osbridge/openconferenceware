f309aa1391e04a09598e6fd93dd2423837727b32
Reid Beels <mail@reidbeels.com>
2013-12-11 12:20:46 -0800

    Set up the host app to load OCW's seeds, via install generator

--- lib/generators/open_conference_ware/install/install_generator.rb
+++ lib/generators/open_conference_ware/install/install_generator.rb
@@ -5,6 +5,16 @@ class OpenConferenceWare::InstallGenerator < Rails::Generators::Base
     copy_file "config_initializer.rb", "config/initializers/open_conference_ware.rb"
   end
 
+  def include_engine_seeds
+    append_to_file "db/seeds.rb" do
+      <<-SEED
+
+# Include OpenConferenceWare's seed data
+OpenConferenceWare::Engine.load_seed
+      SEED
+    end
+  end
+
   def run_setup_rake_task
     rake "open_conference_ware:setup"
   end
