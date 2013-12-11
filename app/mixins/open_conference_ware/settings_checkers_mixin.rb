04292073a6498041d1d65b61b8141ad8ed1a7913
Reid Beels <mail@reidbeels.com>
2013-12-10 16:20:41 -0800

    Add OpenConferenceWare namespace to mixins

--- app/mixins/open_conference_ware/settings_checkers_mixin.rb
+++ app/mixins/open_conference_ware/settings_checkers_mixin.rb
@@ -1,47 +1,50 @@
-# = SettingsCheckersMixin
-#
-# The SettingsCheckersMixin provides methods for checking the status of the
-# SETTINGS, setup via the SettingsReader. These let you write code like
-# +#user_profiles?+ rather than +SETTINGS.has_user_profiles+.
-module SettingsCheckersMixin
+module OpenConferenceWare
 
-  def self.included(mixee)
-    mixee.extend(Methods)
+  # = SettingsCheckersMixin
+  #
+  # The SettingsCheckersMixin provides methods for checking the status of the
+  # settings, setup via the SettingsReader. These let you write code like
+  # +#user_profiles?+ rather than +SETTINGS.has_user_profiles+.
+  module SettingsCheckersMixin
 
-    # Add view helper methods to controllers
-    if mixee.ancestors.include?(ActionController::Base)
-      mixee.class_eval do
-        Methods.instance_methods.each do |name|
-          helper_method(name)
+    def self.included(mixee)
+      mixee.extend(Methods)
+
+      # Add view helper methods to controllers
+      if mixee.ancestors.include?(ActionController::Base)
+        mixee.class_eval do
+          Methods.instance_methods.each do |name|
+            helper_method(name)
+          end
         end
       end
     end
-  end
 
-  module Methods
-    # Create methods like +#event_tracks?+ as wrappers for +SETTINGS.have_event_tracks+.
-    %w[
-      anonymous_proposals
-      event_proposal_comments_after_deadline
-      event_tracks
-      event_session_types
-      events_picker
-      event_rooms
-      multiple_presenters
-      proposal_excerpts
-      proposal_speaking_experience
-      proposal_start_times
-      proposal_statuses
-      user_pictures
-      user_profiles
-    ].each do |name|
-      define_method("#{name}?") do
-        return SETTINGS.send("have_#{name}")
+    module Methods
+      # Create methods like +#event_tracks?+ as wrappers for +OpenConferenceWare.have_event_tracks+.
+      %w[
+        anonymous_proposals
+        event_proposal_comments_after_deadline
+        event_tracks
+        event_session_types
+        events_picker
+        event_rooms
+        multiple_presenters
+        proposal_excerpts
+        proposal_speaking_experience
+        proposal_start_times
+        proposal_statuses
+        user_pictures
+        user_profiles
+      ].each do |name|
+        define_method("#{name}?") do
+          return OpenConferenceWare.send("have_#{name}")
+        end
       end
     end
+
+    include Methods
+    extend Methods
+
   end
-
-  include Methods
-  extend Methods
-
 end
