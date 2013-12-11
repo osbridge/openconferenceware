151202bd087bdb64734b40d3c74f254dc61e9087
Reid Beels <mail@reidbeels.com>
2013-12-11 12:20:14 -0800

    Move creating of development users into db/seeds.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,31 +1,9 @@
 OmniAuth.config.path_prefix = OpenConferenceWare.mounted_path("/auth")
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  require 'openid/store/filesystem' 
+  provider :developer if %w[development preview].include?(Rails.env)
 
-  if %w[development preview].include?(Rails.env) && ActiveRecord::Base.connection.table_exists?('open_conference_ware_authentications')
-    provider :developer
-
-    admin_auth = OpenConferenceWare::Authentication.find_or_initialize_by(provider: :developer, uid: 'admin@ocw.local')
-    admin_auth.name = "Development Admin"
-    admin_auth.email = "admin@ocw.local"
-
-    unless admin_auth.user
-      admin_user = OpenConferenceWare::User.create_from_authentication(admin_auth)
-      admin_user.update_attributes(admin: true, biography: "I am mighty.")
-    end
-
-    mortal_auth = OpenConferenceWare::Authentication.find_or_initialize_by(provider: :developer, uid: 'mortal@ocw.local')
-    mortal_auth.name = "Development User"
-    mortal_auth.email = "mortal@ocw.local"
-
-    unless mortal_auth.user
-      mortal_user = OpenConferenceWare::User.create_from_authentication(mortal_auth)
-      mortal_user.biography = "I'm ordinary."
-      mortal_user.save!
-    end
-  end
-
+  require 'openid/store/filesystem'
   provider :openid, store: OpenID::Store::Filesystem.new(Rails.root.join('tmp'))
   provider :persona
 end
---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+puts "[OCW] Running OpenConferenceWare seeds"
+if %w[development preview].include?(Rails.env)
+  if ActiveRecord::Base.connection.table_exists?('open_conference_ware_authentications')
+    puts "[OCW] Creating sample users for development"
+    admin_auth = OpenConferenceWare::Authentication.find_or_initialize_by(provider: :developer, uid: 'admin@ocw.local')
+    admin_auth.name = "Development Admin"
+    admin_auth.email = "admin@ocw.local"
+
+    unless admin_auth.user
+      admin_user = OpenConferenceWare::User.create_from_authentication(admin_auth)
+      admin_user.update_attributes(admin: true, biography: "I am mighty.")
+    end
+
+    mortal_auth = OpenConferenceWare::Authentication.find_or_initialize_by(provider: :developer, uid: 'mortal@ocw.local')
+    mortal_auth.name = "Development User"
+    mortal_auth.email = "mortal@ocw.local"
+
+    unless mortal_auth.user
+      mortal_user = OpenConferenceWare::User.create_from_authentication(mortal_auth)
+      mortal_user.biography = "I'm ordinary."
+      mortal_user.save!
+    end
+  else
+    puts "[OCW] The open_conference_ware_authentications table does not exist, please run `rake db:migrate`"
+  end
+else
+  puts "[OCW] Not creating development users in the #{Rails.env} environment"
+end
