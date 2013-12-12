a13e246370998dd324c0c90fd2558ce961f740ad
Reid Beels <mail@reidbeels.com>
2013-12-11 17:21:50 -0800

    Dynamically load sign in forms based on OmniAuth providers

--- lib/generators/open_conference_ware/install/templates/omniauth_initializer.rb
+++ lib/generators/open_conference_ware/install/templates/omniauth_initializer.rb
@@ -1,7 +1,19 @@
-OmniAuth.config.path_prefix = OpenConferenceWare.mounted_path("/auth")
+OmniAuth.config.path_prefix = OpenConferenceWare.mounted_path("auth")
 
 Rails.application.config.middleware.use OpenConferenceWare::OmniAuthBuilder do
-  provider :developer if %w[development preview].include?(Rails.env)
+  # Configure authentication providers for OpenConferenceWare
+
+  # The providers below are supported with built-in sign in forms.
+  #
+  # Additional providers can be found at:
+  #   https://github.com/intridea/omniauth/wiki/List-of-Strategies
+  #
+  # When adding a new provider, it will be linked to from the sign in page.
+  #
+  # If you want to display a nicer form, just add a partial at
+  # /app/views/open_conference_ware/authentications/_<provider>.html.erb
+  #
+  # Providers will be shown on the sign in page in the order they are added.
 
   # OpenID
   # add 'omniauth-openid' to Gemfile and uncomment to enable OpenID
@@ -13,4 +25,8 @@ Rails.application.config.middleware.use OpenConferenceWare::OmniAuthBuilder do
   # add 'omniauth-persona' to Gemfile and uncomment to enable Persona
   #
   #provider :persona
+
+  # Developer
+  # Used to provide easy authentication during development
+  provider :developer if %w[development preview].include?(Rails.env)
 end
