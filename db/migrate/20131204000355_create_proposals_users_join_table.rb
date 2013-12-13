3237a2d10f40804c6298fcbd74f127cd5e126c00
Reid Beels <mail@reidbeels.com>
2013-12-12 16:59:51 -0800

    Add indices when creating proposals-users join table

--- db/migrate/20131204000355_create_proposals_users_join_table.rb
+++ db/migrate/20131204000355_create_proposals_users_join_table.rb
@@ -4,5 +4,8 @@ class CreateProposalsUsersJoinTable < ActiveRecord::Migration
       t.integer :proposal_id
       t.integer :user_id
     end
+
+    add_index "open_conference_ware_proposals_users", ["proposal_id"]
+    add_index "open_conference_ware_proposals_users", ["user_id"]
   end
 end
