93429feef6a740a6b8d36a709ec0d0370cd7688e
Reid Beels <mail@reidbeels.com>
2014-07-16 18:24:54 -0700

    Always show room/schedule controls, even for unconfirmed proposals
    Closes #84

--- app/assets/javascripts/open_conference_ware/proposals.js
+++ app/assets/javascripts/open_conference_ware/proposals.js
@@ -54,14 +54,6 @@ function bind_proposal_generic_control(kind, elements) {
           if (matcher) {
             target.html(matcher[1]);
           }
-
-          // Hide or unhide the room and schedule controls based on the proposal's state
-          var dependent = target.parent().parent().find('.proposal_admin_controls_dependent_on_confirmed_status');
-          if (response['proposal_status'] == 'confirmed') {
-            dependent.show();
-          } else {
-            dependent.hide();
-          }
         }
       }
     });
