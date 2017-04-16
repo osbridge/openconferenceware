5969ae7fe26052c0e764150a5f52352ffe1d01aa
Reid Beels <mail@reidbeels.com>
2017-04-15 23:30:31 -0700

    Update proposal status script with OpenConferenceWare namespace

--- util/update_proposal_status_from_voting_spreadsheet.rb
+++ util/update_proposal_status_from_voting_spreadsheet.rb
@@ -8,19 +8,19 @@ def accept_and_reject_from_spreadsheet
   waitlist = %w(   ).map(&:to_i)
 
   reject.each do |id|
-    p = Proposal.find(id)
+    p = OpenConferenceWare::Proposal.find(id)
     p.reject! unless p.rejected?
   end
 
   accept.each do |id|
-    p = Proposal.find(id)
+    p = OpenConferenceWare::Proposal.find(id)
     p.accept! unless p.accepted?
   end
 
   waitlist.each do |id|
-    p = Proposal.find(id)
+    p = OpenConferenceWare::Proposal.find(id)
     p.waitlist! unless p.waitlisted?
   end
 end
 
-accept_and_reject_from_spreadsheet()
+accept_and_reject_from_spreadsheet
